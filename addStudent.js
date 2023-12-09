a5ca3b05518e4f96bca5a18ebc0fc0b278098758
Sabik-adri <126403070+Sabik-adri@users.noreply.github.com>
2023-12-09 23:37:10 +0600

    Add files via upload

---
+++ addStudent.js
@@ -0,0 +1,23 @@
+function goToCourses() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "courses.html";
+  }
+  
+  function goToStudent() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "student.html";
+  }
+
+  function goToTeacher() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "teacher.html";
+  }
+
+  function goToAttendence() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "attendence.html";
+  }
+  function goToExamMarks() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "examMarks.html";
+  }
---
+++ dashboard.js
@@ -0,0 +1,23 @@
+function goToCourses() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "courses.html";
+  }
+  
+  function goToStudent() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "student.html";
+  }
+
+  function goToTeacher() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "teacher.html";
+  }
+
+  function goToAttendance() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "attendance.html";
+  }
+  function goToExamMarks() {
+    // Replace "pageURL" with the URL of the page you want to navigate to
+    window.location.href = "examMarks.html";
+  }
